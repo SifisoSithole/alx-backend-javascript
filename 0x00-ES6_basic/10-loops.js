185abc5b03260568bff2831a927a7f8e7201e916
SifisoSithole <sitholesifisobrian@gmail.com>
2023-03-20 15:02:16 +0200

    append text to elements in an array using for/of loop

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArr = [];
+  for (const ele of array) {
+    newArr.push(appendString + ele);
+  }
+
+  return newArr;
+}
