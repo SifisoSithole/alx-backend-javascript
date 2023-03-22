2a00a33135d96308a670ba942c3ac100ce41c927
SifisoSithole <sitholesifisobrian@gmail.com>
2023-03-22 19:31:24 +0200

    initial commit

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -9,15 +9,4 @@ export default function guardrail(mathFunction) {
   queue.push(value);
   queue.push('Guardrail was processed');
   return queue;
-}export default function guardrail(mathFunction) {
-  const queue = [];
-  let value;
-  try {
-    value = mathFunction();
-  } catch (error) {
-    value = error.toString();
-  }
-  queue.push(value);
-  queue.push('Guardrail was processed');
-  return queue;
 }
